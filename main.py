09e1f8453683aefa0202c2cd34273f02818e7e8d
Sayan <83488026+logansayan@users.noreply.github.com>
2022-05-04 11:09:39 +0530

    show names of people the message sent.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import smtplib
 import random
 
-SENDER_EMAIL = ""
-SENDER_PASSWORD = ""
+SENDER_EMAIL = 'dummyman567@gmail.com'
+SENDER_PASSWORD = 'iamdummy13'
 
 
 def check_birthday(birthday):
@@ -27,15 +27,25 @@ def get_random_quote():
         random_quote = random.choice(all_quotes)
         return random_quote
 
+def main():
+    with open('birthdays.csv', newline='') as csvfile: # OPEN CSV FILE
+        persons = []
+        reader = csv.DictReader(csvfile) 
+        for row in reader: # ITERATE THROUGH ALL THE RECORDS IN CSV
+            person_birthday = concat(row['day'], row['month'])
+            if check_birthday(person_birthday): # CHECK IF PERSON'S BIRTHDAY IS TODAY
+                person_name = row['name']
+                persons.append(person_name)
+                with smtplib.SMTP('smtp.gmail.com', port=587) as connection: # SEND THE EMAIL
+                    connection.starttls()
+                    connection.login(user=SENDER_EMAIL, password=SENDER_PASSWORD)
+                    connection.sendmail(from_addr=SENDER_EMAIL, to_addrs=row['email'], msg=f'Subject: Happy Birthday {person_name}\n\n{get_random_quote()}')
+        if len(persons) >= 1:
+            for i in persons:
+                print(f'Successfully sent message to: {i}')
+        else:
+            print('No one in the \'birthdays.csv\' has birthday today!')
 
-with open('birthdays.csv', newline='') as csvfile: # OPEN CSV FILE
-    reader = csv.DictReader(csvfile) 
-    for row in reader: # ITERATE THROUGH ALL THE RECORDS IN CSV
-        person_birthday = concat(row['day'], row['month'])
-        if check_birthday(person_birthday): # CHECK IF PERSON'S BIRTHDAY IS TODAY
-            with smtplib.SMTP('smtp.gmail.com', port=587) as connection: # SEND THE EMAIL
-                connection.starttls()
-                connection.login(user=SENDER_EMAIL, password=SENDER_PASSWORD)
-                connection.sendmail(from_addr=SENDER_EMAIL, to_addrs=row['email'], msg=f'Subject: Happy Birthday\n\n{get_random_quote()}')
-    print('All messages sent successfully')
+
+main()
 
